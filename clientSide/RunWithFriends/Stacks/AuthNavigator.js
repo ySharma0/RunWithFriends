66852269da8adfc486ad349d3c2323004e2a23f6
User <bridibusher@gmail.com>
2021-01-17 11:36:23 -0800

    Removed files

--- clientSide/RunWithFriends/Stacks/AuthNavigator.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react';
-
-export default function AuthNavigator() {
-    const [user, setUser] = useState(null);
-    
-    
-}
--- clientSide/RunWithFriends/components/AuthContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const loggedIn = false;
-
-export {
-    loggedIn
-};
